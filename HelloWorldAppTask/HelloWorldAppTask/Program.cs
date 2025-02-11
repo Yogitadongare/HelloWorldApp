4fba9ff47d6bc8f73dd5594e757db40bfcaaa8a7
Yogita Dongare <yogitadongare06@gmail.com>
2025-02-11 09:23:45 +0530

    All Changes Added

---
+++ HelloWorldAppTask/HelloWorldAppTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorldAppTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
